5bc3f9d5c63e4a3ace1ea54e30dc3729c2f2e94c
Luca Sepe <luca.sepe@gmail.com>
2020-10-23 11:54:03 +0200

    Add SNS Event example

---
+++ lambda/sns-events/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+// The entry point that runs your Lambda function code.
+func main() {
+	lambda.Start(handler)
+}
+
+// handler is your Lambda handler signature and includes the code which will be executed.
+//
+// See https://docs.aws.amazon.com/lambda/latest/dg/golang-handler.html for valid handler signatures.
+// See https://github.com/aws/aws-lambda-go for event details.
+func handler(ctx context.Context, evt events.SNSEvent) {
+	for _, rec := range evt.Records {
+		el := rec.SNS
+
+		fmt.Printf("[%s %s] Message = %s \n", rec.EventSource, el.Timestamp, el.Message)
+	}
+}
---
+++ lambda/sns-events/sns.tf
@@ -0,0 +1,29 @@
+
+variable "topic_name" {
+  description = "The friendly name for the SNS topic"
+  default     = "test-topic"
+}
+
+//SNS topic to subscribe
+resource "aws_sns_topic" "call_lambda_topic" {
+  name = var.topic_name
+}
+
+resource "aws_lambda_permission" "allow_sns_invoke" {
+  action        = "lambda:InvokeFunction"
+  function_name = var.function_name
+  principal     = "sns.amazonaws.com"
+  statement_id  = "AllowSubscriptionToSNS"
+  source_arn    = aws_sns_topic.call_lambda_topic.arn
+}
+
+resource "aws_sns_topic_subscription" "lambda_subscription" {
+  endpoint  = aws_lambda_function.lambda.arn
+  protocol  = "lambda"
+  topic_arn = aws_sns_topic.call_lambda_topic.arn
+}
+
+output "call_lambda_topic_arn" {
+  description = "The SNS topic ARN to be used for sending messages to invoke Lambda Function"
+  value       = aws_sns_topic.call_lambda_topic.arn
+}
