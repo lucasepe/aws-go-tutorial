4abc8376faba3e28760dbdd1058beec1f8620354
Luca Sepe <luca.sepe@gmail.com>
2020-10-26 10:18:08 +0100

    Refactor API Gateway TF scripts

--- lambda/apigateway-events/apigateway.tf
+++ lambda/apigateway-events/apigateway.tf
@@ -1,16 +1,16 @@
-variable "rest_api_name" {
-  description = "The Name of the associated REST API"
-  default     = "MyDemoAPI"
+variable "api_name" {
+  description = "The name of the associated REST API"
+  default     = "DUMMY_API_NAME"
 }
 
-variable "method" {
-  description = "The HTTP method"
+variable "resource_method" {
+  description = "The resource HTTP method"
   default     = "POST"
 }
 
-variable "path" {
-  description = "The API resource path"
-  default     = "demo"
+variable "resource_path" {
+  description = "The resource path"
+  default     = "{proxy+}"
 }
 
 variable "account_id" {
@@ -18,81 +18,62 @@ variable "account_id" {
   default     = "000000000000"
 }
 
-resource "aws_api_gateway_rest_api" "MyDemoAPI" {
-  name        = "MyDemoAPI"
-  description = "This is my API for demonstration purposes"
+# Creates the ReST API.
+resource "aws_api_gateway_rest_api" "api" {
+  name        = var.api_name
 }
 
-# The API requires at least one "endpoint", or "resource" in AWS terminology.
-# The endpoint created here is: /MyDemoResource
-resource "aws_api_gateway_resource" "MyDemoResource" {
-  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
-  parent_id   = aws_api_gateway_rest_api.MyDemoAPI.root_resource_id
-  path_part   = var.path
+# ReST API requires at least one "endpoint", or "resource" in AWS terminology.
+resource "aws_api_gateway_resource" "resource" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = var.resource_path
 }
 
-resource "aws_api_gateway_method" "MyDemoMethod" {
-  rest_api_id   = aws_api_gateway_rest_api.MyDemoAPI.id
-  resource_id   = aws_api_gateway_resource.MyDemoResource.id
-  http_method   = var.method
+# Defines the resource HTTP method (verb or action).
+resource "aws_api_gateway_method" "resource_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = var.resource_method
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "MyDemoIntegration" {
-  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
-  resource_id = aws_api_gateway_resource.MyDemoResource.id
-  http_method = aws_api_gateway_method.MyDemoMethod.http_method
-  type        = "AWS"
-  uri         = "arn:aws:apigateway:${data.aws_region.current.name}:lambda:path/2015-03-31/functions/${aws_lambda_function.lambda.arn}/invocations"
-  # AWS lambdas can only be invoked with the POST method
+# Integrates the resource (with the specified verb) into the ReST API.
+resource "aws_api_gateway_integration" "resource_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.resource.id
+  http_method             = aws_api_gateway_method.resource_method.http_method
+  type                    = "AWS_PROXY"
   integration_http_method = "POST"
+  uri                     = aws_lambda_function.lambda.invoke_arn
+  #uri        = "arn:aws:apigateway:${data.aws_region.current.name}:lambda:path/2015-03-31/functions/${aws_lambda_function.lambda.arn}/invocations"
 }
 
-resource "aws_api_gateway_method_response" "MyDemoMethodResponse" {
-  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
-  resource_id = aws_api_gateway_resource.MyDemoResource.id
-  http_method = aws_api_gateway_method.MyDemoMethod.http_method
-  status_code = "200"
 
-  response_models = {
-    "application/json" = "Empty"
-  }
+# Deploy the ReST API (i.e. make it publicly available).
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = "stage"
+  depends_on  = [aws_api_gateway_method.resource_method, aws_api_gateway_integration.resource_integration]
 }
 
-resource "aws_api_gateway_integration_response" "MyDemoMethodResponseIntegration" {
-  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
-  resource_id = aws_api_gateway_resource.MyDemoResource.id
-  http_method = aws_api_gateway_method.MyDemoMethod.http_method
-  status_code = aws_api_gateway_method_response.MyDemoMethodResponse.status_code
-
-  response_templates = {
-    "application/json" = ""
-  }
-}
-
+# Gives the API Gatewat permissions to invoke Lambda functions.
 resource "aws_lambda_permission" "allow_api_gateway" {
   function_name = aws_lambda_function.lambda.arn
   statement_id  = "AllowExecutionFromApiGateway"
   action        = "lambda:InvokeFunction"
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:${data.aws_region.current.name}:${var.account_id}:${aws_api_gateway_rest_api.MyDemoAPI.id}/*/${aws_api_gateway_method.MyDemoMethod.http_method}/${aws_api_gateway_resource.MyDemoResource.path}"
+  #source_arn    = "arn:aws:execute-api:${data.aws_region.current.name}:${var.account_id}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.resource_method.http_method}/${aws_api_gateway_resource.resource.path}"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
 }
 
-
-# We can deploy the API now! (i.e. make it publicly available)
-resource "aws_api_gateway_deployment" "hello_api_deployment" {
-  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
-  stage_name  = "stage"
-  depends_on  = [aws_api_gateway_method.MyDemoMethod, aws_api_gateway_integration.MyDemoIntegration]
-  description = "Deploy methods: ${aws_api_gateway_rest_api.MyDemoAPI.name}"
+# Prints the deployes resource URL.
+output "resource_url" {
+  value = "${var.localstack_url}/restapis/${aws_api_gateway_rest_api.api.id}/stage/_user_request_/${aws_api_gateway_resource.resource.path_part}"
 }
 
-output "MyDemoResource_url" {
-  value = "${var.localstack_url}/restapis/${aws_api_gateway_rest_api.MyDemoAPI.id}/stage/_user_request_/${aws_api_gateway_resource.MyDemoResource.path_part}"
-}
-
-
-output "TryWith" {
-  value = "curl --header \"Content-Type: application/json\" --request POST --data '{\"username\":\"xyz\",\"password\":\"xyz\"}' ${var.localstack_url}/restapis/${aws_api_gateway_rest_api.MyDemoAPI.id}/stage/_user_request_/${aws_api_gateway_resource.MyDemoResource.path_part}"
+# Prints a sample curl command (just to do a quick copy an paste test).
+output "curl_command" {
+  value = "curl --header \"Content-Type: application/json\" --request ${var.resource_method} --data '{\"username\":\"xyz\",\"password\":\"xyz\"}' ${var.localstack_url}/restapis/${aws_api_gateway_rest_api.api.id}/stage/_user_request_/${aws_api_gateway_resource.resource.path_part}"
 }
 
